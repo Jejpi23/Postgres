4d9d65c7bbd34d27cbf30e22dd67d7b013524bba
Szymon Bogucki <bogucki.szymon@wp.pl>
2025-02-16 18:28:16 +0100

    created libi

---
+++ C++/database/database.cpp
@@ -0,0 +1,70 @@
+﻿// database.cpp
+#include "database.h"
+
+// Implementacja klasy DatabaseConnection
+DatabaseConnection::DatabaseConnection(const std::string& conninfo) {
+  conn = PQconnectdb(conninfo.c_str());
+  if (PQstatus(conn) != CONNECTION_OK) {
+    std::cerr << "Connection to database failed: " << PQerrorMessage(conn) << std::endl;
+    PQfinish(conn);
+    conn = nullptr;
+  }
+}
+
+DatabaseConnection::~DatabaseConnection() {
+  if (conn) {
+    PQfinish(conn);
+  }
+}
+
+PGconn* DatabaseConnection::getConnection() const {
+  return conn;
+}
+
+bool DatabaseConnection::isConnected() const {
+  return conn != nullptr;
+}
+
+// Implementacja klasy QueryResult
+QueryResult::QueryResult(PGresult* res) : res(res) {}
+
+QueryResult::~QueryResult() {
+  if (res) {
+    PQclear(res);
+  }
+}
+
+bool QueryResult::isValid() const {
+  return res != nullptr;
+}
+
+int QueryResult::getRowCount() const {
+  return PQntuples(res);
+}
+
+int QueryResult::getColumnCount() const {
+  return PQnfields(res);
+}
+
+std::string QueryResult::getValue(int row, int col) const {
+  return PQgetvalue(res, row, col);
+}
+
+// Implementacja klasy database
+database::database() {
+  // Konstruktor (można dodać inicjalizację)
+}
+
+database::~database() {
+  // Destruktor (można dodać sprzątanie)
+}
+
+void database::createtab() {
+  // Implementacja metody createtab
+  // np. wykonanie zapytania SQL do tworzenia tabel
+}
+
+void database::deletetab() {
+  // Implementacja metody deletetab
+  // np. wykonanie zapytania SQL do usuwania tabel
+}
---
+++ C++/database/database.h
@@ -0,0 +1,47 @@
+﻿// database.h
+#ifndef DATABASE_H
+#define DATABASE_H
+
+#include <libpq-fe.h>
+#include <string>
+#include <iostream>
+
+//DatabaseConnection
+class DatabaseConnection {
+public:
+    DatabaseConnection(const std::string& conninfo);
+    ~DatabaseConnection();
+
+    PGconn* getConnection() const;
+    bool isConnected() const;
+
+private:
+    PGconn* conn;
+};
+
+//QueryResult
+class QueryResult {
+public:
+    QueryResult(PGresult* res);
+    ~QueryResult();
+
+    bool isValid() const;
+    int getRowCount() const;
+    int getColumnCount() const;
+    std::string getValue(int row, int col) const;
+
+private:
+    PGresult* res;
+};
+
+//database hard control
+class database {
+public:
+    database();
+    ~database();
+
+    void createtab();
+    void deletetab();
+};
+
+#endif // DATABASE_H
